2a66a806dffcddc047661b41c485ebf8754f6886
Champ Camba <champsupertramp@users.noreply.github.com>
2023-07-14 14:31:28 +0800

    VCard: Fix importing of logo and photo

--- src/um-vcard/src/Core.php
+++ src/um-vcard/src/Core.php
@@ -32,6 +32,9 @@ use JeroenDesloovere\VCard\Property\Title;
 use JeroenDesloovere\VCard\Property\Role;
 use JeroenDesloovere\VCard\Property\Url;
 use JeroenDesloovere\VCard\VCard;
+
+use WP_Filesystem_Direct;
+
 /**
  * Class Core to handle all plugin initialization.
  *
@@ -60,6 +63,13 @@ class Core {
 	 */
 	public function generate( $user_id ) {
 
+		global $wp_filesystem;
+		// Initialize the WP filesystem, no more using 'file-put-contents' function.
+		if ( empty( $wp_filesystem ) ) {
+			require_once ABSPATH . '/wp-admin/includes/file.php';
+			WP_Filesystem();
+		}
+
 		um_fetch_user( $user_id );
 		$lastname   = um_user( 'last_name' );
 		$firstname  = um_user( 'first_name' );
@@ -69,7 +79,7 @@ class Core {
 
 		$user_dir = UM()->uploader()->get_upload_user_base_dir( $user_id ) . DIRECTORY_SEPARATOR;
 
-		$vcard = new VCard( null, Version::version3() );
+		$vcard = new VCard( Kind::individual(), Version::version3() );
 		$vcard->add( new Name( $lastname, $firstname ) );
 		$vcard->add( new Title( um_user( 'title' ) ) );
 		$vcard->add( new Role( um_user( 'role' ) ) );
@@ -134,40 +144,56 @@ class Core {
 		$mobile_number = um_user( 'mobile_number' );
 		if ( $mobile_number ) {
 			$telephone_number = preg_replace( '/[^0-9+]/i', '', $mobile_number );
-			$type            = new Type( 'work' );
-			$value           = new Value( 'text' );
+			$type             = new Type( 'work' );
+			$value            = new Value( 'text' );
 			$vcard->add( new Telephone( $telephone_number, $type, $value ) );
 		}
 
 		$phone_number = um_user( 'phone_number' );
 		if ( $phone_number ) {
 			$telephone_number = preg_replace( '/[^0-9+]/i', '', $phone_number );
-			$type            = new Type( 'home' );
-			$value           = new Value( 'text' );
+			$type             = new Type( 'home' );
+			$value            = new Value( 'text' );
 			$vcard->add( new Telephone( $telephone_number, $type, $value ) );
 		}
 
-		$user_url = um_user( 'user_url' );
+		$user_url = um_user_profile_url();
 		if ( $user_url ) {
 			$vcard->add( new Url( $user_url ) );
 		}
 
 		if ( file_exists( $user_dir . um_profile( 'profile_photo' ) ) && is_file( $user_dir . um_profile( 'profile_photo' ) ) ) {
 
-			$avatar   = $user_dir . um_profile( 'profile_photo' );
-			$data     = file_get_contents( $avatar );
-			// we should split this large string into smaller 72-symbols strings, to correspond the standard.
+			$avatar_dir_path = UM()->uploader()->get_upload_user_base_dir( $user_id ) . DIRECTORY_SEPARATOR . um_profile( 'profile_photo' );
+			$img             = wp_get_image_editor( $avatar_dir_path );
+			$filetype        = wp_check_filetype( $avatar_dir_path );
+			$vcard_filename  = 'vcard-120x120.' . $filetype['ext'];
 
-			$vcard->add( new Photo( $data ) );
-			$vcard->add( new Logo( $data ) );
+			$vcard_avatar_dir_path = UM()->uploader()->get_upload_user_base_dir( $user_id ) . DIRECTORY_SEPARATOR . $vcard_filename;
 
+			try {
+				$img->resize( 120, null, false );
+				$img->set_quality( 100 );
+				$img->save( UM()->uploader()->get_upload_user_base_dir( $user_id ) . DIRECTORY_SEPARATOR . $vcard_filename );
+				$vcard->add( new Photo( $this->image_encode( $vcard_avatar_dir_path ) ) );
+				$vcard->add( new Logo( $this->image_encode( $vcard_avatar_dir_path ) ) );
+			} catch ( \Exception $e ) {
+				wp_die( esc_attr( $e->getMessage() . ' - ' . $vcard_avatar_dir_path ) );
+			}
 		}
 
 		add_action( 'um_vcard_before_save', $vcard, $user_id );
 
 		$formatter = new Formatter( new VcfFormatter(), 'vcard' );
 		$formatter->addVCard( $vcard );
+		$file_type = $filetype['type'];
+		$content   = str_replace( 'PHOTO:data:' . $file_type . ';', 'PHOTO;', $formatter->getContent() );
+		$content   = str_replace( 'LOGO:data:' . $file_type . ';', 'LOGO;', $content );
 		$formatter->save( $user_dir );
+
+		if ( ! $wp_filesystem->put_contents( $user_dir . 'vcard.vcf', $content, 0644 ) ) {
+			return wp_die( esc_attr__( 'Failed to overwrite vcard file', 'um-extended' ) );
+		}
 
 		update_user_meta( $user_id, 'vcard', 'vcard.vcf' );
 
@@ -192,6 +218,20 @@ class Core {
 		);
 
 		return $fields;
+	}
+
+	/**
+	 * Image Encode
+	 *
+	 * @param string $path Vcard File path.
+	 * @return string
+	 */
+	public function image_encode( $path ) {
+		$image    = file_get_contents( $path );
+		$finfo    = new \finfo( FILEINFO_MIME_TYPE );
+		$type     = $finfo->buffer( $image );
+		$big_type = strtoupper( str_replace( 'image/', '', $type ) );
+		return 'data:' . $type . ';ENCODING=b;TYPE=' . $big_type . ':' . base64_encode( $image );
 	}
 
 
