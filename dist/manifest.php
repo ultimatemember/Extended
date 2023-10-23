0cbbd833f7aacc2127d254ef285420bf90d0c093
Champ Camba <champsupertramp@users.noreply.github.com>
2023-10-23 18:31:23 +0800

    Update core assets

--- dist/manifest.php
+++ dist/manifest.php
@@ -2,14 +2,14 @@
 /* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
 $manifestJson = '{
   "app/vue/settings/main.css": {
-    "file": "css/main.43d456ea.css",
+    "file": "css/main.c7d04f28.css",
     "src": "app/vue/settings/main.css"
   },
   "app/vue/settings/main.js": {
     "css": [
-      "css/main.43d456ea.css"
+      "css/main.c7d04f28.css"
     ],
-    "file": "settings-0244aec4.js",
+    "file": "settings-4170bc27.js",
     "isEntry": true,
     "src": "app/vue/settings/main.js"
   }
