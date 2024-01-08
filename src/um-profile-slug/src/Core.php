e4c7944feed65a36bf1bce471109b41c12c8b6c7
Champ Camba <champsupertramp@users.noreply.github.com>
2024-01-08 22:46:20 +0800

    Profile Tab Slug: Fixed subnav links

--- src/um-profile-slug/src/Core.php
+++ src/um-profile-slug/src/Core.php
@@ -20,6 +20,7 @@ class Core {
 
 		add_action( 'init', array( $this, 'rewrite_rules' ), 1 );
 		add_action( 'template_redirect', array( $this, 'redirect' ) );
+		add_action( 'template_redirect', array( $this, 'modify_nav_links' ) );
 	}
 
 	/**
@@ -36,35 +37,6 @@ class Core {
 		$wp->add_query_var( 'subnav' );
 		add_rewrite_rule( $profile_page_slug . '/([~a-z0%-9-]+)/([~a-z0%-9-]+)/([~a-z0%-9-]+)?', 'index.php?pagename=' . $profile_page_slug . '&um_user=$matches[1]&profiletab=$matches[2]&subnav=$matches[3]', 'top' );
 		add_rewrite_rule( $profile_page_slug . '/([~a-z0%-9-]+)/([~a-z0%-9-]+)/?', 'index.php?pagename=' . $profile_page_slug . '&um_user=$matches[1]&profiletab=$matches[2]', 'top' );
-
-		$tabs = UM()->profile()->tabs_active();
-
-		foreach ( $tabs as $tab_id => $tab_data ) {
-			add_filter(
-				'um_profile_menu_link_' . $tab_id,
-				function () use ( $tab_id ) {
-					if ( 'main' === $tab_id ) {
-						return um_user_profile_url();
-					}
-					return um_user_profile_url() . $tab_id;
-				}
-			);
-
-			if ( isset( $tab_data['subnav'] ) ) {
-				foreach ( $tab_data['subnav'] as $id_s => $subtab ) {
-					add_filter(
-						'um_user_profile_subnav_link',
-						function ( $subnav_link, $id_s, $subtab ) use ( $tab_id ) {
-							$subnav_link;
-							$subtab;
-							return um_user_profile_url() . $tab_id . '/' . $id_s;
-						},
-						10,
-						3
-					);
-				}
-			}
-		}
 	}
 
 	/**
@@ -114,6 +86,43 @@ class Core {
 		return $permalink;
 	}
 
+	/**
+	 * Modify Nav Links in Profile forms
+	 */
+	public function modify_nav_links() {
+
+		$tabs       = UM()->profile()->tabs_active();
+		$active_tab = UM()->profile()->active_tab();
+
+		foreach ( $tabs as $tab_id => $tab_data ) {
+			add_filter(
+				'um_profile_menu_link_' . $tab_id,
+				function () use ( $tab_id ) {
+					if ( 'main' === $tab_id ) {
+						return um_user_profile_url();
+					}
+					return um_user_profile_url() . $tab_id;
+				}
+			);
+			if ( isset( $tab_data['subnav'] ) ) {
+				foreach ( $tab_data['subnav'] as $id_s => $subtab ) {
+					if ( get_query_var( 'profiletab' ) !== $tab_id ) {
+						continue;
+					}
+					add_filter(
+						'um_user_profile_subnav_link',
+						function ( $subnav_link, $id_s, $subtab ) use ( $tab_id ) {
+							$subnav_link;
+							$subtab;
+							return um_user_profile_url() . $tab_id . '/' . $id_s;
+						},
+						10,
+						3
+					);
+				}
+			}
+		}
+	}
 	/**
 	 * Get Plugin URL
 	 */
