42fe74c50fcf526350c14036dbdba1b7ab1d555b
Champ Camba <champsupertramp@users.noreply.github.com>
2024-03-07 20:27:52 +0800

    Add MathCaptcha library

--- src/um-math-captcha/src/Core.php
+++ src/um-math-captcha/src/Core.php
@@ -7,7 +7,7 @@
 
 namespace UM_Extended_Math_Captcha;
 
-use Kmlpandey77\MathCaptcha\Captcha;
+use Kmlpandey77\MathCaptcha\Captcha as MathCaptcha;
 
 /**
  * Class Core to handle all plugin initialization.
@@ -17,11 +17,28 @@ use Kmlpandey77\MathCaptcha\Captcha;
 class Core {
 
 	/**
+	 * PHP-based Simple Math Captcha
+	 *
+	 * @var \Kmlpandey77\MathCaptcha\Captcha
+	 */
+	public $cpa;
+
+
+	/**
+	 * Math Captcha field's name
+	 *
+	 * @var string
+	 */
+	public $key = 'um_math_challenge';
+
+	/**
 	 * Init
 	 */
 	public function __construct() {
-		add_action( 'um_submit_form_register', array( $this, 'captcha_validate' ), 1 );
-		add_action( 'um_after_register_fields', array( $this, 'captcha_field' ) );
+		add_action( 'um_submit_form_register', array( $this, 'validate' ), 1 );
+		add_action( 'um_after_register_fields', array( $this, 'add_field' ) );
+
+		$this->cpa = new MathCaptcha();
 	}
 
 	/**
@@ -29,8 +46,8 @@ class Core {
 	 *
 	 * @param array $post_form Form data.
 	 */
-	public function captcha_validate( $post_form ) {
-		if ( ! Captcha::check() ) {
+	public function validate( $post_form ) {
+		if ( ! MathCaptcha::check() ) {
 			UM()->form()->add_error( 'um_math_challenge', __( 'Incorrect answer. Please try again.', 'ultimate-member' ) );
 		}
 	}
@@ -38,12 +55,36 @@ class Core {
 	/**
 	 * Render Captcha Field
 	 */
-	public function captcha_field() {
+	public function add_field() {
 
-		echo esc_attr__( 'Solve this simple Math: ', 'um-math-captcha' ) . esc_attr( new Captcha() ) . ' = ?';
-		echo "<input  type='text' placeholder='" . esc_attr__( 'Your answer...', 'um-math-captcha' ) . "' name='captcha' value=''/>";
-		if ( UM()->form()->has_error( 'um_math_challenge' ) ) {
-			echo wp_kses( UM()->fields()->field_error( UM()->fields()->show_error( 'um_math_challenge' ), 'um_math_challenge' ), UM()->get_allowed_html( 'template' ) );
+		$this->cpa->reset_captcha();
+
+		$field_data = array(
+			'type'        => 'text',
+			'label'       => __( 'Solve this simple Math: ', 'um-math-captcha' ) . $this->cpa->get_captcha_text() . ' = ?',
+			'name'        => $this->key,
+			'help'        => '',
+			'placeholder' => __( 'Your answer...', 'um-math-captcha' ),
+			'icon'        => '',
+			'default'     => null,
+			'required'    => true,
+			'editable'    => true,
+		);
+
+		$data = apply_filters( "um_get_field__{$this->key}", $field_data );
+
+		?>
+			<div class="um-field um-field-text">
+				<?php echo wp_kses( UM()->fields()->field_label( $data['label'], $this->key, $data ), UM()->get_allowed_html() ); ?>
+				<div class="um-field-area">
+					<input type="text" name="<?php echo esc_attr( $data['name'] ); ?>" value="" placeholder="<?php echo esc_attr( $data['placeholder'] ); ?>" />
+				</div>
+			</div>
+		<?php
+
+		if ( UM()->fields()->is_error( $this->key ) ) {
+			$text = UM()->fields()->show_error( $this->key );
+			echo wp_kses( UM()->fields()->field_error( $text, $this->key ), UM()->get_allowed_html() );
 		}
 	}
 }
